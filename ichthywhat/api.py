25f16f95dbff46c71e972a2919af974eb77011a3
Yanir Seroussi <rls@yanirseroussi.com>
2023-04-12 05:09:30 +0000

    Tidy up API

--- ichthywhat/api.py
+++ ichthywhat/api.py
@@ -1,20 +1,25 @@
-import pandas as pd
-from fastapi import FastAPI
-from fastapi import File, UploadFile
-from fastai.learner import load_learner
+"""Basic classification API."""
 from io import BytesIO
+
+import pandas as pd
+from fastai.learner import load_learner
 from fastai.vision.core import PILImage
-from pathlib import Path
+from fastapi import FastAPI, File, UploadFile
 
+from ichthywhat.constants import DEFAULT_RESOURCES_PATH
 
-# TODO: share with streamlit app
-DEFAULT_RESOURCES_PATH = Path(__file__).parent.parent / "resources"
 api = FastAPI()
 _model = load_learner(DEFAULT_RESOURCES_PATH / "model.pkl")
 
 
 @api.post("/predict")
-async def predict(file: UploadFile = File(...)) -> dict[str, float]:
-    image = PILImage.create(BytesIO(await file.read()))
-    preds = _model.predict(image)[2]
-    return pd.Series(data=preds, index=_model.dls.vocab).sort_values(ascending=False).to_dict()
+async def predict(img_file: UploadFile = File(...)) -> dict[str, float]:  # noqa: B008
+    """
+    Return a mapping from species name to score.
+
+    When running with Vagrant, this can be tested with: $ curl -X POST -F "file=@data/demo/P6204455.JPG"
+    http://localhost:9300/predict | jq
+    """
+    image = PILImage.create(BytesIO(await img_file.read()))
+    scores = _model.predict(image)[2]
+    return pd.Series(data=scores, index=_model.dls.vocab).sort_values(ascending=False).to_dict()
---
+++ ichthywhat/constants.py
@@ -0,0 +1,4 @@
+"""Global constants."""
+from pathlib import Path
+
+DEFAULT_RESOURCES_PATH = (Path(__file__).parent.parent / "resources").resolve()
