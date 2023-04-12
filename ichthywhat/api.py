627f3f2447c8e71ff1f0c95086eb7a6d10477efa
Yanir Seroussi <rls@yanirseroussi.com>
2023-04-12 06:15:13 +0000

    Tidy up

--- ichthywhat/api.py
+++ ichthywhat/api.py
@@ -14,12 +14,7 @@ _model = load_learner(DEFAULT_RESOURCES_PATH / "model.pkl")
 
 @api.post("/predict")
 async def predict(img_file: UploadFile = File(...)) -> dict[str, float]:  # noqa: B008
-    """
-    Return a mapping from species name to score.
-
-    When running with Vagrant, this can be tested with: $ curl -X POST -F "file=@data/demo/P6204455.JPG"
-    http://localhost:9300/predict | jq
-    """
+    """Return a mapping from species name to score, based on the given image."""
     image = PILImage.create(BytesIO(await img_file.read()))
     scores = _model.predict(image)[2]
     return pd.Series(data=scores, index=_model.dls.vocab).sort_values(ascending=False).to_dict()
