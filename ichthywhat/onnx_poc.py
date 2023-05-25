34aa102b238a12eda26dea58b56e00d5fb2f3a7a
Yanir Seroussi <rls@yanirseroussi.com>
2023-05-25 05:24:31 +0000

    Tidy up onnx_poc

--- ichthywhat/onnx_poc.py
+++ ichthywhat/onnx_poc.py
@@ -1,79 +1,103 @@
-"""ONNX export and inference proof-of-concept."""
-# TODO: break this up to modules -- export in models.py, inference elsewhere
+"""
+ONNX export and inference proof-of-concept.
+
+Builds on https://community.wandb.ai/t/taking-fastai-to-production/1705
+"""
+import io
+import json
+from pathlib import Path
+
+import numpy as np
+import onnxruntime as ort
+import pandas as pd
+import torch
+import torchvision.transforms
+from fastai.learner import Learner
+from PIL import Image
+
 # TODO: validate exported onnx model on the QUT dataset
 # TODO: refactor streamlit app (use functions) and change it to use ONNX
 # TODO: use ONNX in the API (rewrite Dockerfile accordingly)
 # TODO: try running inference in the browser
-# TODO: check the resizing (different for v2? take centre crop rather than using image.resize?)
-# TODO: try to get the normalise transform args from the learner
-# TODO: inspired by https://community.wandb.ai/t/taking-fastai-to-production/1705 -- reread to ensure it makes sense
-
-import json
-
-import numpy as np
-import onnxruntime as ort
-import torch
-import torchvision
-import torchvision.transforms
-from PIL import Image
 
 
-def onnx_path_to_labels(onnx_path):
-    return onnx_path.parent / f"{onnx_path.name}.labels.json"
+def onnx_model_to_label_path(onnx_model_path: Path) -> Path:
+    """Return the path of the labels json for the given ONNX model path."""
+    return onnx_model_path.parent / f"{onnx_model_path.name}.labels.json"
 
 
-def export_to_onnx(learner, export_path):
+# TODO: move this to models.py to make OnnxWrapper independent of fastai & torch.
+def export_learner_to_onnx(learner: Learner, export_path: Path) -> None:
+    """Export learner to an ONNX model and persist it to export_path."""
     model = torch.nn.Sequential(
-        # Replace fastai's normalisation. Assumes the base model was trained on ImageNet.
+        # TODO: add more steps from OnnxWrapper._load_input_image()?
+        # Replace fastai's normalisation. Tested only with resnet18 (these are ImageNet
+        # stats), so other models might not have the step.
         torchvision.transforms.Normalize(
-            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            mean=learner.dls.after_batch.normalize.mean.squeeze(),
+            std=learner.dls.after_batch.normalize.std.squeeze(),
         ),
         # Actual PyTorch model.
         learner.model.eval(),
         # Replace fastai's softmax layer.
         torch.nn.Softmax(dim=1),
     )
+    # This is a somewhat convoluted way to get the shape of the input, but better than
+    # hard-coding it. There may be a better way to inspect the DataLoaders, but it's
+    # hard with the usual fastai maze of dynamic attributes and methods.
+    input_shape = (
+        learner.dls.test_dl([torch.Tensor([0])], num_workers=0).one_batch()[0].shape
+    )
+    if input_shape[-1] != input_shape[-2]:
+        raise ValueError(
+            "Rectangular images require more tests (see OnnxWrapper._load_input_image)"
+        )
     torch.onnx.export(
         model,
-        # ImageNet default input of 224x224
-        # TODO: needs adjustment for v2?
-        torch.randn(1, 3, 224, 224),
-        export_path,
+        torch.randn(input_shape),
+        str(export_path),
         input_names=["input"],
         output_names=["output"],
-        # Variable length axes.
+        # Allow variable length batches, but still require fixed image sizes.
         dynamic_axes={"input": {0: "batch_size"}, "output": {0: "batch_size"}},
     )
-    with onnx_path_to_labels(export_path).open("w") as fp:
+    with onnx_model_to_label_path(export_path).open("w") as fp:
         json.dump(list(learner.dls.vocab), fp)
 
 
-def image_transform_onnx(path: str, size: int) -> np.ndarray:
-    """Image transform helper for onnx runtime inference."""
-    image = Image.open(path)
-    image = image.resize((size, size))
+class OnnxWrapper:
+    """Simple wrapper around an ONNX image classification model."""
 
-    image = np.array(image)
+    def __init__(self, model_path: Path):
+        """Load the ONNX model and labels."""
+        with onnx_model_to_label_path(model_path).open() as fp:
+            self._labels = json.load(fp)
+        self._ort_sess = ort.InferenceSession(str(model_path))
+        self._img_size = self._ort_sess.get_inputs()[0].shape[2:]
+        self._input_name = self._ort_sess.get_inputs()[0].name
+        self._output_name = self._ort_sess.get_outputs()[0].name
 
-    # Match the exported model input: torch.randn(1, 3, 224, 224)
-    image = image.transpose(2, 0, 1).astype(np.float32)
+    def predict(self, img_path_or_file: Path | io.BytesIO) -> pd.Series:
+        """Return a series mapping labels to sorted predictions for the image."""
+        img = self._load_input_image(img_path_or_file)
+        return pd.Series(
+            data=self._ort_sess.run([self._output_name], {self._input_name: img})[0][0],
+            index=self._labels,
+        ).sort_values(ascending=False)
 
-    image /= 255
-    image = image[None, ...]
-    return image
-
-
-def predict(onnx_model_path, img_path, target_size=224):
-    with onnx_path_to_labels(onnx_model_path).open() as fp:
-        labels = json.load(fp)
-    img = image_transform_onnx(img_path, target_size)
-    sess = ort.InferenceSession(str(onnx_model_path))
-    input_name = sess.get_inputs()[0].name
-    output_name = sess.get_outputs()[0].name
-    input_dims = sess.get_inputs()[0].shape
-    print(
-        f"Input layer name: {input_name}, Output layer name: {output_name}, Input Dimension: {input_dims}"
-    )
-    results = sess.run([output_name], {input_name: img})[0]
-    print(results)
-    print(f"It's a {labels[np.argmax(results)]}", results)
+    def _load_input_image(self, path_or_file: Path | io.BytesIO) -> np.ndarray:
+        # Starting from an RGB image with uint8 values, resize it to self._img_size and
+        # transform it to end up with an array of shape (1, 3, width, height) with
+        # float32 values in the [0, 1] range.
+        # TODO: the transpose step may be wrong for rectangular images (see
+        # TODO: torchvision.transforms.Resize versus PIL's resize)
+        return (
+            (
+                np.array(
+                    Image.open(path_or_file).resize(self._img_size), dtype=np.float32
+                )
+                / 255
+            )
+            .transpose(2, 0, 1)
+            .reshape((1, 3, *self._img_size))
+        )
