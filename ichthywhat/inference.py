746ac76d25bea366f0aa57cc2d79b8ec6bbbb7cd
Yanir Seroussi <rls@yanirseroussi.com>
2023-06-19 15:15:45 +1000

    Move more operations into the ONNX model (#24)

--- ichthywhat/inference.py
+++ ichthywhat/inference.py
@@ -4,10 +4,8 @@ Thin ONNX wrapper for inference in production.
 Originally inspired by https://community.wandb.ai/t/taking-fastai-to-production/1705
 """
 import io
-import json
 from collections.abc import Sequence
 from pathlib import Path
-from typing import Any
 
 import numpy as np
 import pandas as pd
@@ -21,19 +19,14 @@ class OnnxWrapper:
     def __init__(self, model_path: Path):
         """Load the ONNX model and prepare for inference."""
         self._ort_sess = InferenceSession(str(model_path))
-        self._labels = json.loads(
-            self._ort_sess.get_modelmeta().custom_metadata_map["labels"]
-        )
-        self._img_size = self._ort_sess.get_inputs()[0].shape[2:]
         self._input_name = self._ort_sess.get_inputs()[0].name
         self._output_name = self._ort_sess.get_outputs()[0].name
 
     def predict(self, img_path_or_file: Path | io.BytesIO) -> pd.Series:
         """Return a series mapping labels to sorted predictions for the image."""
-        img = self._load_input_image(img_path_or_file)
+        img = np.array(Image.open(img_path_or_file), dtype=np.uint8)
         return pd.Series(
-            data=self._ort_sess.run([self._output_name], {self._input_name: img})[0][0],
-            index=self._labels,
+            self._ort_sess.run([self._output_name], {self._input_name: img})[0][0]
         ).sort_values(ascending=False)
 
     def evaluate(
@@ -43,6 +36,9 @@ class OnnxWrapper:
         accuracy_top_ks: Sequence[int] = (1, 3, 10),
     ) -> dict[str, float]:
         """Return a mapping from k to accuracy@k for the given paths & labels."""
+        # Note: this can be done more efficiently by batching images, but one image at
+        # a time is good enough given that this function is only run for evaluation
+        # purposes. Also, batch support was removed from the model for simplicity.
         correct_at_k = {k: 0 for k in accuracy_top_ks}
         for image_path, label in zip(image_paths, labels, strict=True):
             predictions = self.predict(image_path)
@@ -53,22 +49,3 @@ class OnnxWrapper:
             f"top_{k}_accuracy": correct / len(image_paths)
             for k, correct in correct_at_k.items()
         }
-
-    def _load_input_image(
-        self, path_or_file: Path | io.BytesIO
-    ) -> np.ndarray[Any, np.dtype[np.float32]]:
-        # Starting from an RGB image with uint8 values, resize it to self._img_size and
-        # transform it to end up with an array of shape (1, 3, width, height) with
-        # float32 values in the [0, 1] range.
-        # TODO: the transpose step may be wrong for rectangular images (see
-        # TODO: torchvision.transforms.Resize versus PIL's resize)
-        return (
-            (
-                np.array(
-                    Image.open(path_or_file).resize(self._img_size), dtype=np.float32
-                )
-                / 255
-            )
-            .transpose(2, 0, 1)
-            .reshape((1, 3, *self._img_size))
-        )
--- ichthywhat/training.py
+++ ichthywhat/training.py
@@ -1,5 +1,4 @@
 """Training and persistence for models that get served."""
-import json
 from pathlib import Path
 
 import onnx
@@ -65,11 +64,46 @@ def train_app_model(
     learner.export(exported_model_path)
 
 
+class _RgbUint8ImgToTensor(torch.nn.Module):
+    """Convert uint8 RGB image to a float32 batch in [0, 1].
+
+    Input shape: H x W x C
+    Output shape: 1 x C x H x W
+    """
+
+    def forward(self, rgb_uint8_img: torch.Tensor) -> torch.Tensor:
+        return (rgb_uint8_img.to(torch.float32) / 255).permute(2, 0, 1).unsqueeze(0)
+
+
+class _SqueezeBatch(torch.nn.Module):
+    """Squeeze out the batch to go from shape 1 x classes to a flat array."""
+
+    def forward(self, batch: torch.Tensor) -> torch.Tensor:
+        return batch.squeeze(0)
+
+
 def export_learner_to_onnx(learner_path: Path, export_path: Path) -> None:
     """Export learner to an ONNX model and persist it to export_path."""
     learner = load_learner(learner_path)
+    # This is a somewhat convoluted way to get the shape of the input, but better than
+    # hard-coding it. There may be a better way to inspect the DataLoaders, but it's
+    # hard with the usual fastai maze of dynamic attributes and methods.
+    batch_size, num_channels, height, width = (
+        learner.dls.test_dl([torch.Tensor([0])], num_workers=0).one_batch()[0].shape
+    )
+    assert batch_size == 1
+    assert num_channels == 3
+    if height != width:
+        raise ValueError(
+            "Rectangular inputs require more testing (unsure about the dimension order)"
+        )
     model = torch.nn.Sequential(
-        # TODO: add more steps from OnnxWrapper._load_input_image()?
+        # Assume we're working with one RGB image at a time rather than batches.
+        _RgbUint8ImgToTensor(),
+        # ONNX exports with antialias=True are unsupported with opset_version=16, but
+        # antialias=False yields the same performance on the QUT dataset as using PIL's
+        # resizing.
+        torchvision.transforms.Resize((height, width), antialias=False),
         # Replace fastai's normalisation. Tested only with resnet18 (these are ImageNet
         # stats), so other models might not have the step.
         torchvision.transforms.Normalize(
@@ -80,34 +114,48 @@ def export_learner_to_onnx(learner_path: Path, export_path: Path) -> None:
         learner.model.eval(),
         # Replace fastai's softmax layer.
         torch.nn.Softmax(dim=1),
+        # Remove the batch dimension added by the model to keep the output simple.
+        _SqueezeBatch(),
     )
-    # This is a somewhat convoluted way to get the shape of the input, but better than
-    # hard-coding it. There may be a better way to inspect the DataLoaders, but it's
-    # hard with the usual fastai maze of dynamic attributes and methods.
-    input_shape = (
-        learner.dls.test_dl([torch.Tensor([0])], num_workers=0).one_batch()[0].shape
-    )
-    if input_shape[-1] != input_shape[-2]:
-        raise ValueError(
-            "Rectangular images require more tests (see OnnxWrapper._load_input_image)"
-        )
     torch.onnx.export(
         model,
-        torch.randn(input_shape),
+        # Sample input: Height and width don't really matter because they're dynamic.
+        torch.randint(
+            low=0,
+            high=255,
+            size=(height // 2, width * 3, num_channels),
+            dtype=torch.uint8,
+        ),
         str(export_path),
-        input_names=["input"],
-        output_names=["output"],
-        # Allow variable length batches, but still require fixed image sizes.
-        dynamic_axes={"input": {0: "batch_size"}, "output": {0: "batch_size"}},
+        input_names=["rgb_image"],
+        output_names=["class_probabilities"],
+        # Allow input images of any size, but still require a fixed number of channels.
+        dynamic_axes={"rgb_image": {0: "height", 1: "width"}},
         # Use the maximum supported version.
         opset_version=16,
     )
     # Looks like there isn't an easy way to specify the labels as part of the export.
     # See: https://github.com/pytorch/pytorch/issues/42808
+    # Instead, we add a ZipMap that turns the list of predicted probabilities into a
+    # mapping from class name to probability.
     onnx_model = onnx.load(str(export_path))
-    onnx_model.metadata_props.append(
-        onnx.StringStringEntryProto(  # type: ignore[attr-defined]
-            key="labels", value=json.dumps(list(learner.dls.vocab))
+    # Need to explicitly add the opsetid and domain to use ZipMap.
+    # See https://stackoverflow.com/a/68504343
+    onnx_model.opset_import.append(onnx.helper.make_opsetid("ai.onnx.ml", 1))
+    zipmap_node = onnx.helper.make_node(
+        "ZipMap",
+        inputs=[onnx_model.graph.output[0].name],
+        outputs=["class_to_probability"],
+        classlabels_strings=list(learner.dls.vocab),
+        domain="ai.onnx.ml",
+    )
+    onnx_model.graph.node.append(zipmap_node)
+    # Remove the output info set by torch.onnx.export() and set it to the zipmap_node.
+    # See https://github.com/microsoft/onnxruntime/issues/1455#issuecomment-514805365
+    onnx_model.graph.output.pop()
+    onnx_model.graph.output.append(
+        onnx.helper.ValueInfoProto(  # type: ignore[attr-defined]
+            name=zipmap_node.output[0]
         )
     )
     onnx.save(onnx_model, str(export_path))
